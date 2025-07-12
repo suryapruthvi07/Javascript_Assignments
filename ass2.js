50028e1c6661c5ecbba8f5d224de7be0beaafc7d
suryapruthvi07 <suryavanshipruthviraj881@gmail.com>
2025-07-12 11:49:36 +0530

    Add files via upload

---
+++ ass2.js
@@ -0,0 +1,39 @@
+//Assignment 2 Q-1-Build a basic calculator with two numbers and print the result for all
+//arithmetic operations.
+
+var num1 = 20;
+var num2 = 5;``
+
+var addition = num1 + num2;
+var subtraction = num1 - num2;
+var multiplication = num1 * num2;
+var division = num1 / num2;
+var modulus = num1 % num2;
+
+console.log(num1);
+console.log(num2);
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(modulus);
+
+//Assignment 2 Q-2 Create two string variables (name, city) and log a message using a
+//template literal.
+
+let name="pruthvi";
+let city="baramati";
+
+console.log(`my name is:${name},my city is:${city}`)
+
+//Assignment 2 Q-3
+
+let result1 = 5 + " apples";
+console.log(result1);
+
+let result2 = true + 3;
+console.log(result2);
+
+let result3 = "Login status: " + false;
+console.log(result3);
+

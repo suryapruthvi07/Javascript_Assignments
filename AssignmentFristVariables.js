0947b61db8c1e765907ced81d8051012fe87e880
suryapruthvi07 <suryavanshipruthviraj881@gmail.com>
2025-07-12 11:21:52 +0530

    Add files via upload

---
+++ AssignmentFristVariables.js
@@ -0,0 +1,21 @@
+let name="pruthvi"; 
+console.log(name);//pruthvi 
+console.log(typeof name);   //string
+
+let age=21;
+console.log(age);//21
+console.log(typeof age);//number
+
+let isStudent = true;
+console.log(isStudent);//true
+console.log(typeof isStudent);//boolean
+
+let emptyValue = null;
+console.log(emptyValue);        //null
+console.log(typeof emptyValue);  //  object
+
+
+let notAssigned;
+console.log(notAssigned);       //  undefined
+console.log(typeof notAssigned); //  undefined
+

c4d71f7dcb244a3e6f90adfa49d5ce7e75d5d92d
suryapruthvi07 <suryavanshipruthviraj881@gmail.com>
2025-07-12 12:04:17 +0530

    Add files via upload

---
+++ ass4.js
@@ -0,0 +1,26 @@
+//Assignment 4 Q-1
+
+function greet(name) {
+  return `${name} JavaScript`;
+}
+let message = greet("Pruthvi");
+console.log(message);
+
+
+//Assignment 4 Q-2
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+console.log(isEven(7)); 
+
+
+//Assignment 4 Q-3
+
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+
+console.log(add(5, 3));       
+console.log(subtract(5, 3));  
+console.log(multiply(5, 3));  

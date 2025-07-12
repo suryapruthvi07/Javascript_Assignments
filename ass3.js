cd3144947328addc2f6ace47426a96b8286f38af
suryapruthvi07 <suryavanshipruthviraj881@gmail.com>
2025-07-12 11:58:49 +0530

    Add files via upload

---
+++ ass3.js
@@ -0,0 +1,23 @@
+//assignment 3 Q-1 Write a program using if-else to check voting eligibility based on
+//age.
+
+
+let age = 17;
+
+if (age >= 18) {
+  console.log("You are eligible to vote");
+} else {
+  console.log("You are not eligible to vote");
+}
+
+
+//ass3 Q-2 Use a for loop to print multiplication table of 7
+
+let num=7;
+
+for(let i=1; i<=10; i++){
+    console.log(i*num);
+}
+
+
+
